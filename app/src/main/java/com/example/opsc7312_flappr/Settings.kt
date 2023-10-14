ebd13a661b438708bf2e76862156a424a93c1ba7
TheHoundAt10 <93482445+TheHoundAt10@users.noreply.github.com>
2023-10-14 17:14:42 +0200

    Front end for settings page done, Attempt at backend too

--- app/src/main/java/com/example/opsc7312_flappr/Settings.kt
+++ app/src/main/java/com/example/opsc7312_flappr/Settings.kt
@@ -1,11 +1,59 @@
 package com.example.opsc7312_flappr
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.Switch
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class Settings : AppCompatActivity() {
+    // Declare variables to store user preferences
+    private var isMetric = true
+    private var maxDistance = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        // Initialize views
+        val switchUnits = findViewById<Switch>(R.id.switchUnits)
+        val sbMaxDistance = findViewById<SeekBar>(R.id.sbMaxDistance)
+        val etMaxDistance = findViewById<EditText>(R.id.etMaxDistance)
+
+        // Set listeners for the switch and seekbar
+        switchUnits.setOnCheckedChangeListener { buttonView, isChecked ->
+            isMetric = isChecked
+            updateMaxDistanceLabel(isMetric)
+        }
+        sbMaxDistance.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                maxDistance = progress
+                etMaxDistance.setText(maxDistance.toString())
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
+
+        // Set listener for the Save button
+        findViewById<View>(R.id.btnSaveSettings).setOnClickListener { v: View? ->
+            // Save user preferences to variables or perform desired actions
+            saveUserPreferences(isMetric, maxDistance)
+        }
+    }
+
+    private fun updateMaxDistanceLabel(isMetric: Boolean) {
+        val tvUnits = findViewById<TextView>(R.id.tvUnits)
+        if (isMetric) {
+            tvUnits.text = "Kilometres"
+        } else {
+            tvUnits.text = "Miles"
+        }
+    }
+
+    private fun saveUserPreferences(isMetric: Boolean, maxDistance: Int) {
+        // Implement saving user preferences to variables or storage here
     }
 }
