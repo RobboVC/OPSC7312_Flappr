eac63a2ec110589a9b00366cbaf67793b2f9cdc7
AdamJSwain <adamjswain@gmail.com>
2023-10-14 18:56:28 +0200

    Added partially broken code to Login and Register pages, login now requires username and password ("Admin", "Password"), being able to set your own username and password does not work.

--- app/src/main/java/com/example/opsc7312_flappr/Login.kt
+++ app/src/main/java/com/example/opsc7312_flappr/Login.kt
@@ -1,16 +1,50 @@
 package com.example.opsc7312_flappr
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        var givenUsername = findViewById<TextView>(R.id.emailEditText).text
+        var givenPassword = findViewById<TextView>(R.id.etPassword).text
+        val i = getIntent()
+        val username = i.getStringExtra("username")
+        val password = i.getStringExtra("password")
+        val AdminUsername = "Admin"
+        val AdminPassword = "Admin"
+        val name = i.getStringExtra("name")
+        val btnGoToReg = findViewById<Button>(R.id.btnGoToRegister)
+
+        btnGoToReg.setOnClickListener(){
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+        }
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener(){
+            if(givenUsername.toString().equals(username.toString()) && givenPassword.toString().equals(password.toString())) {
+                redirectToNavigationDrawer()
+            }
+            else if(givenUsername.toString().equals("Admin") && givenPassword.toString().equals("Password")) {
+                redirectToNavigationDrawer()
+            }
+            else{
+                Toast.makeText(this, "There is no account with this username and password combination" + username + password, Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+
     }
-    fun redirectToNavigationDrawer(view: View) {
+    fun redirectToNavigationDrawer() {
         val intent = Intent(this, HomePage::class.java)
         startActivity(intent)
     }
--- app/src/main/java/com/example/opsc7312_flappr/Register.kt
+++ app/src/main/java/com/example/opsc7312_flappr/Register.kt
@@ -1,11 +1,31 @@
 package com.example.opsc7312_flappr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 
 class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        val txtUsername = findViewById<TextView>(R.id.emailEditText)
+        val txtPassword = findViewById<TextView>(R.id.etPassword)
+        val txtName = findViewById<TextView>(R.id.etName)
+        var username = txtUsername.text
+        var password = txtPassword.text
+        var name = txtName.text
+        val btnLogin = findViewById<TextView>(R.id.tvSubtitle)
+
+        btnLogin.setOnClickListener(){
+            val intent = Intent(this, Login::class.java)
+            intent.putExtra("name", name)
+            intent.putExtra("username", username)
+            intent.putExtra("password", password)
+            startActivity(intent)
+        }
+
     }
 }
